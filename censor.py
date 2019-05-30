01877fff28e2306598b328623a053bfa68e74dfa
StuartCowley <47369259+StuartCowley@users.noreply.github.com>
2019-05-30 10:34:55 +0100

    Refactored to python 3

--- censor.py
+++ censor.py
@@ -1,5 +1,3 @@
-#! /usr/bin/env python
-
 def censor(text, word):
     words = text.split()
     result = ''
@@ -9,13 +7,12 @@ def censor(text, word):
         if i == word:
             words[count] = stars
         count += 1
-    result =' '.join(words)
+    result = ' '.join(words)
     return result
 
 def user():
-  phrase = raw_input("Feed me the dirty phrase you want cleaning up: ")
-  block = raw_input("Enter the word you want censoring: ")
-  print
-  print censor(phrase,block)
+    phrase = input("Feed me the dirty phrase you want cleaning up: ")
+    block = input("Enter the word you want censoring: ")
+    print(censor(phrase, block))
 
 user()
