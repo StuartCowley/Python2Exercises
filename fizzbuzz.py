7ecadd82a5ca9485f6762a275086f76b5250d157
StuartCowley <47369259+StuartCowley@users.noreply.github.com>
2019-05-30 10:30:44 +0100

    Refactored to python3

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,18 +1,15 @@
-#! /usr/bin/env python
-
-user = int(raw_input("Enter top of range: "))
-
-list = range(1,user+1)
+user = int(input("Enter top of range: "))
+list = range(1, user+1)
 
 def game(list):
     for i in list:
         if (i % 3 == 0 and i % 5 == 0):
-            print "fizzbuzz"
+            print("fizzbuzz")
         elif i % 3 == 0:
-            print "fizz"
+            print("fizz")
         elif i % 5 == 0:
-            print "buzz"
+            print("buzz")
         else:
-            print i
+            print(i)
 
 game(list)
