16bb04ad0862e4bceec1330de7c93662668fb96b
StuartCowley <47369259+StuartCowley@users.noreply.github.com>
2019-05-30 10:56:09 +0100

    Refactored to python 3

--- vowel remover.py
+++ vowel remover.py
@@ -1,12 +1,11 @@
-#! /usr/bin/env python
-
-text = raw_input("Enter a word: ")
+text = input("Enter a word: ")
 
 def anti_vowel(text):
     result = ""
     vowels = "ieaouIEAOU"
     for char in text:
-          if char not in vowels:
+        if char not in vowels:
             result += char
     return result
-print anti_vowel(text)
+
+print(anti_vowel(text))
