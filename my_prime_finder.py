3fb4b3f5009da763dff961aab7a873675fd8ed03
StuartCowley <47369259+StuartCowley@users.noreply.github.com>
2019-05-30 11:27:30 +0100

    Refactored to python 3

--- my_prime_finder.py
+++ my_prime_finder.py
@@ -1,22 +1,21 @@
-#! /usr/bin/env python
-
 # Code has been refactored to make more efficient for larger numbers
 # Even numbers > 2 now not calculated, numbers > number/2 not calculated
 
 # Ask user for a number
-number = int(raw_input("Enter your number: "))
+number = (int(input("Enter your number: ")))
 # Reduce workload by removing numbers greater than number/2
-half = range(1,(number/2)+1)
+half = range(1, int(number/2))
 # List of factors of number
 result = []
 
 # Generate list of factors of number and append number to end
+
 def prime_factors():
     # Ignore even numbers
     if number == 2:
-        print "Your number is a prime, factors are 1 and 2"
+        print("Your number is a prime, factors are 1 and 2")
     elif number > 1 and number % 2 == 0:
-        print "Your number is divisible by 1, 2, and itself so is NOT a prime"
+        print("Your number is divisible by 1, 2, and itself so is NOT a prime")
     # Generate factors for odd numbers
     elif number % 2 != 0:
         for i in half:
@@ -25,10 +24,11 @@ def prime_factors():
         result.append(number)
         # Return result to user
         if len(result) == 2:
-            print "Your number is prime, factors are 1 and %s" % (str(result[1]))
+            print("Your number is prime, factors are 1 and %s" %
+                  (str(result[1])))
         else:
-            print "Your number is NOT a prime, factors are: "
+            print("Your number is NOT a prime, factors are: ")
             for i in result:
-                print i,
+                print(i,)
 
 prime_factors()
