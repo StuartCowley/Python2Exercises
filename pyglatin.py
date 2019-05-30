1e4bbf3b84b77af0b301de710f9f80d3f51325fb
StuartCowley <47369259+StuartCowley@users.noreply.github.com>
2019-05-30 10:43:23 +0100

    Refactored to python 3

--- pyglatin.py
+++ pyglatin.py
@@ -2,14 +2,13 @@
 
 # This translator only works for single words. See pyglatin_phrase for improved version
 
-print "Welcome to the pyg Latin translator!"
-blank=""
-word=raw_input("Give me a word: ")
+print("Welcome to the pyg Latin translator!")
+blank = ""
+word = input("Give me a word: ")
 
-if word!=blank and word.isalpha():
-  first=word[0]
-  restofword=word[1:len(word)]
-  print restofword+first+str("ay")
+if word != blank and word.isalpha():
+    first = word[0]
+    restofword = word[1:len(word)]
+    print(restofword+first+str("ay"))
 else:
-  print "You must enter a real word"
-
+    print("You must enter a real word")
