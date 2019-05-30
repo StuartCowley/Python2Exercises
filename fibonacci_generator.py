fdb3c5d29cda87549172e41877f8da553e6cde03
StuartCowley <47369259+StuartCowley@users.noreply.github.com>
2019-05-30 10:33:19 +0100

    Refactored to python 3

--- fibonacci_generator.py
+++ fibonacci_generator.py
@@ -1,14 +1,13 @@
-#! /usr/bin/env python
-
-# Ask user for a number of values to generate and print to console
 def get_input():
-    num = int(raw_input("Enter how many values of the fibonacci sequence would you like: "))
+    num = int(
+        input("Enter how many values of the fibonacci sequence would you like: "))
     return num
 
 # Start of Fibonacci sequence
-list = [0,1]
+list = [0, 1]
 
 # Generate the fibonacci sequence to number of iterations required
+
 def fibonacci(val):
     while val > 1:
         new = list[len(list)-1] + list[len(list)-2]
@@ -20,6 +19,6 @@ def fibonacci(val):
 fibonacci(get_input())
 
 # Print result
-print "The Fibonacci sequence to %s places is: " % (len(list)-1)
+print("The Fibonacci sequence to %s places is: " % (len(list)-1))
 for i in list[1:]:
-    print i,
+    print(i)
